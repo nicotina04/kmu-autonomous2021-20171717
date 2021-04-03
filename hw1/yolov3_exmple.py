1720ced093bea0aa9dfe446bda661a667da645c8
nicotina04 <devherdson@gmail.com>
2021-04-03 16:51:54 +0900

    Update: Update annotation
    Update annotation

--- hw1/yolov3_exmple.py
+++ hw1/yolov3_exmple.py
@@ -12,13 +12,19 @@ def get_env():
 
 
 if __name__ == "__main__":
+    """Print my environment"""
     get_env()
+
+    """Import object_detection (YOLO)"""
     from cvlib.object_detection import draw_bbox
 
     try:
-        img_path = ''
+        """Read an image file and predict"""
+        img_path = 'doll.jpeg'
         img_read = cv2.imread(img_path)
+        """Detect objects"""
         bbox, label, conf = cvlib.detect_common_objects(img_read)
+        """Print and draw the result"""
         print(bbox, label, conf)
         cv2.imwrite('result.jpg', draw_bbox(img_read, bbox, label, conf))
     except AttributeError:
